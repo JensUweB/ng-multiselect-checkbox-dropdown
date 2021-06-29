370b710c30da9ed4789b7152e616a80fd7c13b07
JensUweB <jens.becker@root-itsolutions.de>
2021-06-29 16:56:00 +0200

    refactor: improve multi-select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
-  list : any[];
+  list: any[];
 
-  constructor(){
-    this.list = 
-      [
-        {name :'India',id : '1'},
-        {name :'US',id : '2'},
-        {name :'China',id : '3'},
-        {name :'France',id : '4'}
-      ]
+  groupLabels = {
+    '67/1': 'Spezial',
+    '67/2': 'Nahrungsmittel'
+  };
+
+  constructor() {
+    this.list = [
+      { name: 'Kurzware, Wolle', id: '1', group: '67/1' },
+      { name: 'Waren des täglichen Bedarfs', id: '2', group: '67/1' },
+      { name: 'Uhren, Schmuck, Geschenkartikel', id: '3', group: '67/1' },
+      { name: 'Besondere Waren des täglichen Bedarfs', id: '4', group: '67/1' },
+      { name: 'Obst, Gemüse', id: '5', group: '67/2' },
+      { name: 'Eier, Nudeln', id: '6', group: '67/2' },
+      { name: 'Fleisch, Wurst', id: '7', group: '67/2' },
+      { name: 'Geflügel', id: '8', group: '67/2' }
+    ];
   }
 
-  shareCheckedList(item:any[]){
+  shareCheckedList(item: any[]) {
     console.log(item);
   }
-  shareIndividualCheckedList(item:{}){
+  shareIndividualCheckedList(item: {}) {
     console.log(item);
   }
-
 }
--- src/app/multi-select-dropdown/multi-select-dropdown.component.ts
+++ src/app/multi-select-dropdown/multi-select-dropdown.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-multi-select-dropdown',
   templateUrl: './multi-select-dropdown.component.html',
   styleUrls: ['./multi-select-dropdown.component.scss']
 })
-export class MultiSelectDropdownComponent {
+export class MultiSelectDropdownComponent implements OnInit {
   @Input() data: any[];
   @Input() selected: any[] = [];
   @Input() labelKey = 'title';
@@ -13,42 +13,92 @@ export class MultiSelectDropdownComponent {
   @Input() groupBy: string;
   @Input() maxSelection = -1;
   @Input() onlyOneGroupSelectable = false;
+  @Input() groupLabels: any;
 
   @Output() shareSelected = new EventEmitter();
   @Output() shareCurrentSelected = new EventEmitter();
 
-  currentSelected: {};
+  groups: string[] = [];
+  group: string;
+  showDropDown = false;
 
-  constructor() {
-  }
+  constructor() {}
 
-  getSelectedValue(status: Boolean, value) {
-    if (status) {
-      this.selected.push(value);
-    } else {
-      var index = this.selected.indexOf(value);
-      this.selected.splice(index, 1);
+  ngOnInit() {
+    if (this.groupBy) {
+      this.data.forEach(item => {
+        if (!this.groups.some(a => a === item[this.groupBy])) {
+          this.groups.push(item[this.groupBy]);
+          if (!this.group) {
+            this.group = item[this.groupBy];
+          }
+        }
+      });
     }
-
-    this.currentSelected = { checked: status, item: value };
-
-    //share checked list
-    this.shareSelected.emit(this.selected);
-
-    //share individual selected item
-    this.shareCurrentSelected.emit(this.currentSelected);
   }
 
+  /**
+   * Returns an array that contains all items that match the given group
+   */
+  getByGroup(group: string) {
+    return this.data.filter(item => item[this.groupBy] === group);
+  }
+
+  /**
+   * Item toggle - adds or removes the item based on this.selected
+   */
   updateSelection(item) {
-    if (this.selected.some(a => a[this.dataKey] === item[this.dataKey] )) {
-      this.selected.splice(this.selected.indexOf(item), 1);
-    } else if (this.maxSelection < 0 || this.selected.length < this.maxSelection){
+    if (this.selected.some(a => a[this.dataKey] === item[this.dataKey])) {
+      this.removeItem(item);
+    } else if (
+      this.maxSelection < 0 ||
+      this.selected.length < this.maxSelection
+    ) {
       this.selected.push(item);
+      if (!this.group) {
+        this.group = item[this.groupBy];
+      }
     }
   }
 
+  /**
+   * Checks if the provided item can be selected or not
+   */
   canSelectItem(item) {
-    return this.selected.some(a => a[this.dataKey] === item[this.dataKey] ||
-      (this.maxSelection > 0 && this.selected.length < this.maxSelection));
+    if (this.group && this.onlyOneGroupSelectable) {
+      return (
+        (item[this.groupBy] === this.group &&
+          (this.maxSelection > 0 &&
+            this.selected.length < this.maxSelection)) ||
+        this.isSelected(item) ||
+        this.maxSelection < 0
+      );
+    }
+    // TODO: fix
+    // This second condition does not work properly
+    return (
+      (this.maxSelection > 0 && this.selected.length < this.maxSelection) ||
+      this.isSelected(item) ||
+      this.maxSelection < 0
+    );
+  }
+
+  /**
+   * Checks if the provided item is in this.selected
+   */
+  isSelected(item) {
+    return this.selected.some(a => a[this.dataKey] === item[this.dataKey]);
+  }
+
+  /**
+   * Removes the provided item from this.selected
+   */
+  removeItem(item) {
+    this.selected = this.selected.filter(
+      a => a[this.dataKey] !== item[this.dataKey]
+    );
+    if (this.selected.length === 0) {
+      this.group = null;
+    }
   }
 }
